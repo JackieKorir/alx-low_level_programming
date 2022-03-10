7362c763a7b78c7af68a80aeb4e7cc9b7fb03df1
JackieKorir <cjacklyne@rocketmail.com>
2022-03-10 11:34:54 -0800

    Hello. puts

---
+++ 0x00-hello_world/4-pits.c
@@ -0,0 +1,11 @@
+#include <stdio>
+
+/**
+ * main - prints the phrase "Programming is like building a multilingual puzzle"
+ * Return : 0 if exited proerly, none-zero otherwise
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
